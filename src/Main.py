507a15a0ae70b0fe2eca6d80d24a050c48ff9ef0
domeniksabitzer08-create <domeniksabitzer08@gmail.com>
2026-02-11 11:00:45 +0100

    basic inplemented basic movement

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,9 @@
+from logging.handlers import DEFAULT_TCP_LOGGING_PORT
+
 import pygame
 import snakeGame
 
-from snakeGame import Vector2D, Grid, screen_to_world_pos
+from snakeGame import Vector2D, Grid, screen_to_world_pos, DeltaTime, MovementManager
 
 
 import time
@@ -11,13 +13,15 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 running = True
 
+
+### DEBUG FUNCTIONS ###
 def show_grid():
     for i in range(Grid.cell_count):
         for j in range(Grid.cell_count):
-            cell = snakeGame.Parts(0, Vector2D(i*Grid.cell_size , j*Grid.cell_size))
-            cell.render(screen)
-            time.sleep(0.001)
-            pygame.display.update()
+            cell = pygame.Rect(i * Grid.cell_size, j* Grid.cell_size, 30, 30)
+            pygame.draw.rect(screen, (200,0,0), cell)
+
+
 def debug_grid_with_mouse():
     mouse_pos = pygame.mouse.get_pos()
     mouse_grid_pos = screen_to_world_pos(Vector2D(mouse_pos[0], mouse_pos[1]))
@@ -31,13 +35,46 @@ def debug_grid_with_part():
     part_grid_pos = screen_to_world_pos(part.position)
     print(f"screen pos: {part.position}")
     print(f"Grid_pos x: {part_grid_pos.x}, Gris_pos y: {part_grid_pos.y}")
-show_grid()
 
+### RENDERING FUNCTIONS ###
+def clear_screen():
+    screen.fill((0, 0, 0))
+
+def render_parts(part_list: iter):
+    for part in part_list:
+        part.render(screen, color=(255, 255, 255))
+
+### START ###
+n_starting_parts = 3
+part_list = []
+
+first_part = snakeGame.Parts(0, Vector2D(80, 52))
+movement_manager = MovementManager(first_part, 1, DeltaTime.get_delta_time()) # Todo-- delta time is static, use the Delta_time directly in the move-function of Parts class
+
+
+for i in range(n_starting_parts):
+    part_list.append(snakeGame.Parts(i+1, Vector2D(80-(Grid.cell_size*(i+1)) , 52)))
+    movement_manager.add_part(part_list[i])
+    for j in range(i):
+        part_list[i].movement_list.append(movement_manager.direction)
+
+speed = 0.01
+
+### Update ###
 while running:
+    tick = pygame.time.get_ticks()
+    clear_screen()
+    show_grid()
+    movement_manager.handle_input()
+    first_part.render(screen, color=(255, 255, 255))
+    render_parts(part_list)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if tick % 600 == 0:
+        movement_manager.tick()
     pygame.display.update()
+
 
 
--- src/snakeGame.py
+++ src/snakeGame.py
@@ -1,13 +1,16 @@
+import time
+from time import sleep
+
 import pygame
 from dataclasses import dataclass
 
+from fontTools.svgLib.path import PathBuilder
+from pygame.examples.go_over_there import delta_time
 
 
+### MANAGEMENT CLASSES ###
 
-
-
-
-@dataclass
+@dataclass(frozen=True)
 class Vector2D:
         x: float
         y: float
@@ -28,10 +31,28 @@ class Vector2D:
             if isinstance(other, (int, float)):
                 return Vector2D(self.x / other, self.y / other)
 
+# Vector directions
+Vector2D.left = Vector2D(-1, 0)
+Vector2D.right = Vector2D(1, 0)
+Vector2D.up = Vector2D(0, -1)
+Vector2D.down = Vector2D(0, 1)
+
+class RefreshRate:
+    refresh_rate = 120
+
+class DeltaTime:
+    @staticmethod
+    def get_delta_time() -> float:
+        """Returns the delta_time depending on the refresh_rate"""
+        clock = pygame.time.Clock()
+        delta_time = clock.tick(RefreshRate.refresh_rate)
+        delta_time = max(0.001,min(0.1,delta_time))
+        return delta_time
+
 class Grid:
     start_pos = Vector2D(0, 0)
-    cell_count = 13
-    cell_size = 30
+    cell_count = 10
+    cell_size = 50
 
 
 
@@ -40,7 +61,6 @@ def screen_to_world_pos(pos: Vector2D):
     # 1. find the nearest cell position
     nearest_pos_x = int(pos.x / Grid.cell_size)
     nearest_pos_y = int(pos.y / Grid.cell_size)
-
     return Vector2D(nearest_pos_x, nearest_pos_y)
 
 
@@ -55,9 +75,56 @@ class Parts:
     def __init__(self, index: int, start_pos: Vector2D):
         self.index = index
         self.position = start_pos
+        self.grid_pos = screen_to_world_pos(start_pos)
+        self.movement_list = []
+
     def move(self, direction: Vector2D):
-        self.position += direction
+        self.position += direction * Grid.cell_size
+        self.grid_pos = screen_to_world_pos(self.position)
+
+
     def render(self, screen, color = (255,0,0)):
-        part = pygame.Rect(self.position.x,self.position.y,10,10)
+        part = pygame.Rect(self.grid_pos.x * Grid.cell_size,self.grid_pos.y * Grid.cell_size,30,30)
         pygame.draw.rect(screen,color,part)
+
+class MovementManager:
+    def __init__(self, first_part: Parts, speed: float, delta_time: float):
+        self.direction = Vector2D(1, 0) # the direction of the first_part
+        self.first_part = first_part
+        self.snake = []
+        self.speed = speed
+        self.delta_time = delta_time
+    def tick(self):
+        time.sleep(0.1)
+        # add the direction of the first part, move the part and delete the saved movement
+        self.first_part.move(self.direction)
+        for part in self.snake:
+            part.movement_list.append(self.direction)
+            # if part.movement_list[0] != part.movement_list[1]: # Only for debugging
+            part.move(part.movement_list[0])
+            part.movement_list.pop(0)
+
+    def change_direction(self, new_direction: Vector2D):
+        if self.check_movement(new_direction):
+            self.direction = new_direction
+
+    def add_part(self, part: Parts):
+        self.snake.append(part)
+        part.movement_list.append(self.direction)
+
+    def check_movement(self, new_direction: Vector2D):
+        """checks if the movement change can be executed"""
+        if (new_direction * -1) == self.direction:
+            return False
+        else:
+            return True
+    def handle_input(self):
+        if pygame.key.get_pressed()[pygame.K_w]:
+            self.change_direction(Vector2D.up)
+        if pygame.key.get_pressed()[pygame.K_s]:
+            self.change_direction(Vector2D.down)
+        if pygame.key.get_pressed()[pygame.K_d]:
+            self.change_direction(Vector2D.right)
+        if pygame.key.get_pressed()[pygame.K_a]:
+            self.change_direction(Vector2D.left)
 
